7bc621824c1c06adfea325941a89e887821f4fc4
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-09-20 19:28:48 +0200

    modal
    create modal

--- FishEye/photographers.js
+++ FishEye/photographers.js
@@ -22,30 +22,6 @@ function fetchData() {
 
       let filteredUser = filteredUsers[0];
 
-       //TOTTAL LIKES COUNTER - for class="galerie__infoDown"
-       //array of numbers
-
-        //price     
-        // const reducedPrice = filteredUsers.map((filteredUsers) =>filteredUsers.price);
-
-        // console.log(reducedPrice);
-
-        // const reducedPrice = filteredUsers.map(item => item.price).reduce((prev, curr) => prev + curr);
-        // console.log(reducedPrice);
-
-       
-
-        // //likes COUNT
-
-        // const allLikes = filteredUsers.map(item => item.likes).reduce((prev, curr) => prev + curr);
-        // console.log(allLikes);
-
-        // const reducedLikes = filteredUsers.map((filteredUser) =>filteredUser.likes);
-
-        // console.log(reducedLikes);
-          
- 
-
       //make user TAG SEPARATE in innerHTML
       let tagsHtml = filteredUser.tags
         .map((tag) => {
@@ -55,9 +31,12 @@ function fetchData() {
         })
         .join("");
 
+      
+
+     
       //make user TAG SEPARATE in innerHTML add this  `+tagsHtml+`
       //make user div in innerHTML
-      //BUTTON CONCTACT ME mozda izmedju?
+      //BUTTON CONCTACT ME 
 
       document.getElementById("photographer").innerHTML =
            `<div class = "filteredUser__info"> 
@@ -68,10 +47,54 @@ function fetchData() {
                   <p class="price">${filteredUser.price}</p>
                   <div class="tags__all">`+tagsHtml+`</div>
             </div> 
-            <div class="filteredUser__btn"><button class="button">Contatez-moi</button></div>
+            <div class="filteredUser__btn"><button  data-modal-target="#modal" class="button" onclick="openModalButtons()">Contatez-moi</button></div>
           <div class = "filteredUser__img">
             <a href="photographer-page.html?id=${filteredUser.id}"> <img id="profile" src="Documents/Sample Photos/Photographers ID Photos/${filteredUser.portrait}" class="profile"  alt=""/> </a>
           </div>`;
+
+
+
+      // //createMODAL NE RADI
+      // document.getElementById("modalBox").innerHTML =
+      // `  <div class="modal">
+      //         <div class="modalForm" id="modalForm">
+      //           <h2 class="contactName">Contactez-moi<br>${filteredUser.name}</h2>
+      //           <button data-close-button class="close-button">&times;</button>
+      //         </div>
+      //     </div>
+      //     <div id="overlay"></div>
+      // `;
+
+      //   const openModalButtons = document.querySelectorAll("data-modal-target");
+      //   const closeModalButtons = document.querySelectorAll("data-close-button");
+      //   const overlay = document.getElementById("overlay");
+
+      //   openModalButtons.forEach(button => {
+      //     button.addEventListener("click", () => {
+      //       const modal = document.querySelector(button.dataset.modalTarget)
+      //       openModal(modal)
+      //     })
+      //   })
+
+      // closeModalButtons.forEach(button => {
+      //   button.addEventListener("click", () => {
+      //     const modal = button.closest(".modal")
+      //     closeModal(modal)
+      //   })
+      // })
+      
+      // function openModal(modal) {
+      //   if (modal == null) return
+      //   modal.classlist.add("active")
+      //   overlay.classList.add("active")
+      // }
+
+      // function closeModal(modal) {
+      //   if (modal == null) return
+      //   modal.classlist.remove("active")
+      //   overlay.classList.remove("active")
+      // }
+
 
 
       //sort MEDIA BY group  
@@ -79,18 +102,6 @@ function fetchData() {
       let photographerPhotos = data.media.filter((media) => {
         return media.photographerId == photographerId;
       });
-      
-      //ALL LIKES total 
-
-      let totalLikes = 0;
-      photographerPhotos.forEach(item => {
-        totalLikes = totalLikes + item.likes;
-      });
-      console.log(totalLikes);
-
-      
-
-
       
       if (sortBy) {
         photographerPhotos = photographerPhotos.sort((a, b) => {
@@ -104,18 +115,80 @@ function fetchData() {
         });
       }
 
+      console.log(photographerPhotos);
+
+
+      
+      //ALL LIKES total 
+
+      let totalLikes = 0;
+      photographerPhotos.forEach(item => {
+        totalLikes = totalLikes + item.likes;
+      });
+      console.log(totalLikes);
+
+
+      
+
+      // const sortedByPop = filteredUsers.sort(function (a, b){
+      //   return a.likes - b.likes;
+      // })
+
+      // console.log(sortedByPop);
+
+
+
+
+      //dropdownmenu
+
+      document.getElementById("dropdownmenu").innerHTML =
+      `
+          <div class="menuDrop">
+            <p>Trier par</p>
+           <button id="sort__Pop" class="buttonMenu" onclick="myFunctionPop()">Popularité</button>
+           <button id="sort__Date" class="buttonMenu">Date</button>
+          <button id="sort__Name" class="buttonMenu">Titre</button>
+          </div>
+      `;
+
+      
+
+      //display all LIKES DOWN RIGHT  add down  in mediaID
+      document.getElementById("downright").innerHTML =
+      ` <div class="galerie__infoDown"> 
+            <div class="HeartLIKES">
+            <div class="numberLikes" id="incrimentText">` + totalLikes + ` </div>
+            <div class="galerie__likes"><i class="fas fa-heart"></i></div>
+            <p class="galerie__infotitle">${filteredUser.price}€/jour</p>
+        </div> 
+      `;
+
+
       //display all  MEDIA PHOTOS VIDEOS in inner html
 
       //make variable for MEDIAhtml VIDEO if else 
 
       let photoHtml = photographerPhotos
         .map((photo) => {
+
+        // //display all LIKES counter 
+        //   let likeEvery =
+        //   ` <div > 
+        //         <div class="HeartLIKES">
+        //         <div class="numberLikes" id="incrimentText">${photo.likes} </div>
+        //         <button class="galerie__likes" onclick="incrimentButton()" value="Increment"><i class="fas fa-heart"></i></button>
+        //     </div> 
+        //   `;
+        //   console.log(likeEvery);
+
+
+        //If Else show img or video
           let mediaHtml = "";
 
           if (photo.hasOwnProperty("video")) {
-            mediaHtml = `<video id="img" class="galerie__img" src="Documents/Sample Photos/${filteredUser.name}/${photo.video}"  alt=""/>`;
+            mediaHtml = `<video id="img" class="galerie__img" src="/FishEye/Documents/Sample Photos/${filteredUser.name}/${photo.video}"  alt=""/>`;
           } else {
-            mediaHtml = `<img id="img" class="galerie__img" src="Documents/Sample Photos/${filteredUser.name}/${photo.image}"  alt=""/>`;
+            mediaHtml = `<img id="img" class="galerie__img" src="/FishEye/Documents/Sample Photos/${filteredUser.name}/${photo.image}"  alt=""/>`;
           }
 
           return (
@@ -128,25 +201,14 @@ function fetchData() {
                 <p class="photoDate">${photo.date}</p>
                 <p class="photoPrice">${photo.price}</p>
             </div>
-
             <div class="galerie__info"> 
                     <p class="galerie__title">${photo.title}</p>
-                    <div class="HeartLIKES">
-                      <div class="numberLikes" id="incrimentText">${photo.likes}</div>
-                      <button class="galerie__likes" onclick="incrimentButton()" value="Increment"><i class="fas fa-heart"></i></button>
-                    </div>
-
-
-             </div> 
-
-          </div>
-          <div class="galerie__infoDown"> 
-                 <div class="HeartLIKES">
-                  <div class="numberLikes" id="incrimentText">` + totalLikes + ` </div>
+                <div class="HeartLIKES">
+                  <div class="numberLikes" id="incrimentText">${photo.likes} </div>
                   <button class="galerie__likes" onclick="incrimentButton()" value="Increment"><i class="fas fa-heart"></i></button>
-               </div>
-                 <p class="galerie__infotitle">${filteredUser.price}€/jour</p>
-           </div> 
+                </div> 
+              </div> 
+          </div>
           `
           );
         })
@@ -181,6 +243,7 @@ fetchData();
       console.log(value);
       document.getElementById("incrimentText").innerHTML = value;
     }
+    
 
     // function incrLikes(e) {
     //   let likes = parseInt(e.target.innerHTML);
@@ -196,108 +259,6 @@ fetchData();
       window.location.search = urlParams;
     }
 
-//BUTTON CONCTACT ME
-
-    // // 1. Create the button
-    // let button = document.createElement("button");
-    // button.innerHTML = "Contatez-moi";
-
-    // // 2. Append somewhere
-    // let body = document.getElementById("btn");
-    // body.appendChild(button);
-
-    // // 3. Add event handler
-
-    // //get CONTACT ME BUTTON MODAL
-
-    // document.getElementById("btn").addEventListener("click",
-    //   function () {
-    //     document.querySelector(".bg-modal").style.display = "flex";
-    //   });
-
-
-    // // 4. Close modal by X
-
-    // document.querySelector(".button__send").addEventListener("click",
-    // function () {
-    //   document.querySelector(".bg-modal").style.display = "none";
-    // });
-
-
-    // // 5. Close modal by send button
-
-    // document.querySelector(".close").addEventListener("click",
-    // function () {
-    // document.querySelector(".bg-modal").style.display = "none";
-    // alert("I am an alert box!");
-    // });
-
-
-  //   //create BUTtONS FOR FILTER GALERY
-
-  //   // 1. Create the button
-  //   function menuButton() {
-  //     let menuLikes = document.getElementById("btnLikes");
-       
-  //     // creating button element
-  //     let buttonLikes = document.createElement('BUTTON');
-       
-  //     // creating text to be
-  //     //displayed on button
-  //     let text = document.createTextNode("Popularité");
-       
-  //     // appending text to button
-  //     button.appendChild(text);
-       
-  //     // appending button to div
-  //     menuLikes(.appendChild(buttonLikes);
-  // }
-
-
-//fetch json and display all  MEDIA in inner html
-
-// function fetchMedia() {
-//     fetch("FishEyeData.json")
-//     .then(response => {
-//       return response.json();
-//     })
-//     .then(data => {
-//       console.log(data.media);
-//       const html = data.media.map(user => {
-//         return `
-//         <div class="galerie__user">
-
-//                 <div class = "galerie__image">
-//                     <p>${user.id}</p>
-//                     <p>${user.photographerId}</p>
-//                     <img id="img" class="galerie__img" src="Documents/Sample Photos/Tracy/${user.image}"  alt=""/>
-//                     <video class="galerie__video"src="Documents/Sample Photos/Tracy/${user.video}" type="video/mp4">controls</video>
-//                     <p>${user.tags}</p>
-//                     <p>${user.date}</p>
-//                     <p>${user.price}</p>
-//                 </div>
-
-//                  <div class="galerie__info">
-//                         <p class="galerie__title">${user.title}</p>
-//                         <p class="galerie__likes">${user.likes}</p>
-//                 </div>
-
-//         </div>
-//         `;
-//       })
-//       .join("");
-//       console.log(html)
-//       document.querySelector("#media").innerHTML = (html)
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     })
-//   }
-
-//   fetchMedia();
-
-
-
 // //IncrimentHEART
 
 //       function incrimentButton() {
@@ -307,3 +268,25 @@ fetchData();
 //         console.log(value);
 //         document.getElementById("incrimentText").innerHTML = value;
 //       }
+
+
+//menu FILTER SORT
+
+
+// function myFunctionPop() {
+//   likes.sort();
+//   document.getElementById("media").innerHTML = likes;
+// };
+
+// const media = totalLikes;
+
+// const sortedByPop = media.sort(function (a, b){
+//   return a.likes - b.likes;
+// })
+
+// console.log(sortedByPop);
+
+
+
+
+//MODAL
