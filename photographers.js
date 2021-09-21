a7877543160738c6b80cfdac25260d5409f2b29b
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-09-21 10:12:43 +0200

    modalbox
    organisation of modal box

--- photographers.js
+++ photographers.js
@@ -47,7 +47,7 @@ function fetchData() {
                   <p class="price">${filteredUser.price}</p>
                   <div class="tags__all">`+tagsHtml+`</div>
             </div> 
-            <div class="filteredUser__btn"><button  data-modal-target="#modal" class="button" onclick="openModalButtons()">Contatez-moi</button></div>
+            <div class="filteredUser__btn"><button data-modal-target="#modalBox" class="button">Contatez-moi</button></div>
           <div class = "filteredUser__img">
             <a href="photographer-page.html?id=${filteredUser.id}"> <img id="profile" src="Documents/Sample Photos/Photographers ID Photos/${filteredUser.portrait}" class="profile"  alt=""/> </a>
           </div>`;
@@ -55,26 +55,29 @@ function fetchData() {
 
 
       // //createMODAL NE RADI
-      // document.getElementById("modalBox").innerHTML =
-      // `  <div class="modal">
-      //         <div class="modalForm" id="modalForm">
-      //           <h2 class="contactName">Contactez-moi<br>${filteredUser.name}</h2>
-      //           <button data-close-button class="close-button">&times;</button>
-      //         </div>
-      //     </div>
-      //     <div id="overlay"></div>
-      // `;
+      document.getElementById("modal").innerHTML =
+      `  
+              <div class="modal-header" id="modal-header">
+                <h2 class="contactName">Contactez-moi<br>${filteredUser.name}</h2>
+                <button data-close-button class="close-button">&times;</button>
+              </div>
+              <div class=modal-body>
+              <h2>BLAH BLAH BLAH BLAH</h2>
+              </div>
+          
+          <div id="overlay"></div>
+      `;
 
-      //   const openModalButtons = document.querySelectorAll("data-modal-target");
-      //   const closeModalButtons = document.querySelectorAll("data-close-button");
-      //   const overlay = document.getElementById("overlay");
+        const openModalButtons = document.querySelectorAll("[data-modal-target]");
+        //const closeModalButtons = document.querySelectorAll("data-close-button");
+       // const overlay = document.getElementById("overlay");
 
-      //   openModalButtons.forEach(button => {
-      //     button.addEventListener("click", () => {
-      //       const modal = document.querySelector(button.dataset.modalTarget)
-      //       openModal(modal)
-      //     })
-      //   })
+        openModalButtons.forEach(button => {
+          button.addEventListener("click", () => {
+            const modal = document.querySelector(button.dataset.modalTarget)
+            openModal(modal)
+          })
+        })
 
       // closeModalButtons.forEach(button => {
       //   button.addEventListener("click", () => {
@@ -83,11 +86,11 @@ function fetchData() {
       //   })
       // })
       
-      // function openModal(modal) {
-      //   if (modal == null) return
-      //   modal.classlist.add("active")
-      //   overlay.classList.add("active")
-      // }
+      function openModal(modal) {
+        if (modal == null) return
+        modal.classlist.add("active")
+        overlay.classList.add("active")
+      }
 
       // function closeModal(modal) {
       //   if (modal == null) return
@@ -139,8 +142,7 @@ function fetchData() {
 
 
 
-      //dropdownmenu
-
+      //DROPDOWN MENU
       document.getElementById("dropdownmenu").innerHTML =
       `
           <div class="menuDrop">
@@ -165,7 +167,6 @@ function fetchData() {
 
 
       //display all  MEDIA PHOTOS VIDEOS in inner html
-
       //make variable for MEDIAhtml VIDEO if else 
 
       let photoHtml = photographerPhotos
@@ -186,9 +187,9 @@ function fetchData() {
           let mediaHtml = "";
 
           if (photo.hasOwnProperty("video")) {
-            mediaHtml = `<video id="img" class="galerie__img" src="/Documents/Sample Photos/${filteredUser.name}/${photo.video}"  alt=""/>`;
+            mediaHtml = `<video id="img" class="galerie__img" src="Documents/Sample Photos/${filteredUser.name}/${photo.video}"  alt=""/>`;
           } else {
-            mediaHtml = `<img id="img" class="galerie__img" src="/Documents/Sample Photos/${filteredUser.name}/${photo.image}"  alt=""/>`;
+            mediaHtml = `<img id="img" class="galerie__img" src="Documents/Sample Photos/${filteredUser.name}/${photo.image}"  alt=""/>`;
           }
 
           return (
