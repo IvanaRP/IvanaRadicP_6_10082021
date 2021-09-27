dd68de0b5c071aedccc834a980ac92804ac9132a
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-09-27 18:15:52 +0200

    deleted modal
    moved modal in modal.js

--- photographers.js
+++ photographers.js
@@ -59,125 +59,13 @@ function fetchData() {
             <a href="photographer-page.html?id=${filteredUser.id}"> <img id="profile" src="Documents/Sample Photos/Photographers ID Photos/${filteredUser.portrait}" class="profile"  alt=""/> </a>
           </div>`;
 
-
-      //MODAL open on CONTACT ME button
-      document.getElementById("modalBox").innerHTML =
-      `  <div class="modal" id="modal">
-              <div class="modal-header" id="modal-header">
-                <h2 class="contactName">Contactez-moi<br>${filteredUser.name}</h2>
-                <button class="close-button">&times;</button>
-              </div>
-              <div class=modal-body>
-              <form action"/" method="GET" id="form" class="form">
-              
-                <div class="modalplace">
-                  <label for="firstname" class="firstname">Prenom</label>
-                  <input id="firstname" name="firstname" class="firstname" type="text" placeholder="Prenom"  minlength="2"> 
-                  <div id="error1" class="error">Veuillez entrer 2 caractères ou plus pour le champ du prenom.</div>
-                </div>
-                <div class="modalplace">
-                  <label for="lastname" class="lastname">Nom</label>
-                  <input id="lastname" name="lastname" class="lastname"type="text" placeholder="Nom" minlength="2">
-                  <div id="error2" class="error">Veuillez entrer 2 caractères ou plus pour le champ du nom.</div>
-                </div>
-                <div class="modalplace">
-                  <label for="email" class="email">E-mail</label>
-                  <input id="email" name="email" type="text" class="email" placeholder="Email">
-                  <div id="error3" class="error">Entrez une adresse valide. Exemple : contact@nom.com</div>
-                </div>
-                <div class="modalplace">
-                  <label for="message" class="message">Votre message</label>
-                  <input id="message" name="message" class="messageinput" type="text" placeholder="Votre message" minlength="2">
-                  <div id="error4" class="error">Entrez votre message.</div>
-                </div>
-                <div class="modalplace">
-                  <button id="send" class="button" type="submit">Envoyez</button>
-                </div>
-              </form>
-              
-              </div>
-          </div>
-          <div id="overlay" class="overlay"></div>
-      `;
-      
+      // open MODAL
       document.getElementById("open").addEventListener("click",
       function() {
           document.querySelector(".modalBox").style.display = "flex";
       });
 
-      document.querySelector(".close-button").addEventListener("click",
-      function() {
-          document.querySelector(".modalBox").style.display = "none";
-      });
-
-      const firstname = document.getElementById("firstname");
-      const lastname = document.getElementById("lastname");
-      const email = document.getElementById("email");
-      const message = document.getElementById("message");
-      const form = document.getElementById("form");
-
-      let mailformat = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-
-      let error1 = document.getElementById("error1");
-      let error2 = document.getElementById("error2")
-      let error3 = document.getElementById("error3")
-      let error4 = document.getElementById("error4")
      
-      form.addEventListener("submit", (e) => {
-      /*conditin IF ELSE for FIRST name input*/
-      if (firstname.value == "" || firstname.value.lenght < 2) {
-        e.preventDefault();//stop form from submitting
-        error1.style.display = "inline-block"
-        
-        return false;
-      } else {
-        
-        error1.style.display = "none"
-      }
-
-      /*conditin IF ELSE for LAST name input*/
-
-      if (lastname.value == "" || lastname.value.lenght < 2) {
-        e.preventDefault();//stop form from submitting
-        
-        error2.style.display = "inline-block"
-        
-        return false;
-      }  else {
-        
-        error2.style.display = "none"
-      }
-
-      /*conditin IF ELSE for EMAIL input*/
-
-      if (!email.value.match(mailformat)){
-        e.preventDefault();//stop form from submitting
-        error3.style.display = "inline-block"
-        return false;
-
-      } else {
-        
-        error3.style.display = "none"
-      }
-
-      /*conditin IF ELSE for MESSAGE*/
-
-      if (message.value == "" || message.value.lenght < 2) {
-        e.preventDefault();//stop form from submitting
-        
-        error4.style.display = "inline-block"
-        
-        return false;
-        
-      }   else {
-        
-        alert("Merci ! Votre réservation a été reçue.")
-        error4.style.display = "none"
-        
-      }
-
-      });
-
       // Display photographer MEDIA by id
       let photographerPhotos = data.media.filter((media) => {
         return media.photographerId == photographerId;
@@ -266,29 +154,29 @@ const btnTitle = document.getElementById("title");
 
 // almost working
 
-const sortedByLike = photographerPhotos.sort(function (a, b) {
-  return b.likes - a.likes;
-});
-
-document.getElementById("popular").addEventListener("click", sortPopular);
-document.getElementById("date").addEventListener("click", sortDate);
-document.getElementById("title").addEventListener("click", sortTitle);
-const sortGaler = document.getElementById("media");
 // const sortedByLike = photographerPhotos.sort(function (a, b) {
 //   return b.likes - a.likes;
-//   console.log(sortedByLike);
+// });
 
-function sortPopular() {
-  btnPopular.innerHTML = "Date";
-  // document.getElementById("galerie").innerHTML = sortedByLike;
-// sortGaler.innerHTML = sortedByLike;
-}
-function sortDate() {
-  btnPopular.innerHTML = "Popular";
-}
-function sortTitle() {
-  btnPopular.innerHTML = "Date";
-}
+// document.getElementById("popular").addEventListener("click", sortPopular);
+// document.getElementById("date").addEventListener("click", sortDate);
+// document.getElementById("title").addEventListener("click", sortTitle);
+// const sortGaler = document.getElementById("media");
+// // const sortedByLike = photographerPhotos.sort(function (a, b) {
+// //   return b.likes - a.likes;
+// //   console.log(sortedByLike);
+
+// function sortPopular() {
+//   btnPopular.innerHTML = "Date";
+//   // document.getElementById("galerie").innerHTML = sortedByLike;
+// // sortGaler.innerHTML = sortedByLike;
+// }
+// function sortDate() {
+//   btnPopular.innerHTML = "Popular";
+// }
+// function sortTitle() {
+//   btnPopular.innerHTML = "Date";
+// }
 // console.log(sortGaler);
 
 // function sortLike () {
@@ -297,11 +185,11 @@ function sortTitle() {
 // console.log(sortLike);
 
 
-     // //SORTED BY LIKES  WORKs!!!
+     //SORTED BY LIKES  WORKs!!!
 //      const sortedByLike = photographerPhotos.sort(function (a, b) {
 //       return b.likes - a.likes;
 //  });
-    //  console.log(sortedByLike);
+//      console.log(sortedByLike);
 
 //ADD BUTTON TO SORT !!!doesnt work
 //       const btnSortPop = document.getElementById("sortPop1");
@@ -411,16 +299,14 @@ function sortTitle() {
 // let likes = photo.likes;
 // console.log(likes);
  
-//LIKES COUNTER class="INCRIMENT" PROBA3
-// document.getElementById("likesAlone").innerHTML =
-// `   <div class="HeartLIKES">
-//         <div class="numberLikes" id="incrimentText3" value=${photo.likes}>${photo.likes} </div>
-//         <button class="galerie__likes" onclick="incrimentButton3()" ><i class="fas fa-heart"></i></button>
-//     </div> 
+// LIKES COUNTER class="INCRIMENT" PROBA3
+document.getElementById("likesAlone").innerHTML =
+`   <div class="HeartLIKES">
+        <div class="numberLikes" id="incrimentText3" value=${photo.likes}>${photo.likes}</div>
+        <button class="galerie__likes" onclick="incrimentButton3()" ><i class="fas fa-heart"></i></button>
+    </div> 
     
-// `;
-
-
+`;
 
 
       //If Else show img or video
@@ -451,7 +337,7 @@ function sortTitle() {
                       </div> 
 
                       <div class="HeartLIKES">
-                        <div class="numberLikes" id="incrimentText" >${photo.likes} </div>
+                        <div class="numberLikes" id="incrimentText">${photo.likes} </div>
                         <button class="galerie__likes" id="incrimentText" onclick="incrimentButton()" ><i class="fas fa-heart"></i></button>
                       </div> 
                  </div> 
@@ -527,7 +413,7 @@ fetchData();
 
  
     // onClick = sortPhotosBy('likes'); 
-    //SEARCH SORT BY LIKES
+    // SEARCH SORT BY LIKES
     // function sortPhotosBy(sortBy) {
     //   const urlParams = new URLSearchParams(window.location.search);
     //   urlParams.set("sort", sortBy);
@@ -537,11 +423,11 @@ fetchData();
 
 
  
-// // COUNTER OF LIKES - IncrimentHEART PROBA3
-// function incrimentButton3() {
-//   let element = document.getElementById("incrimentText3");
-//   let value = element.innerHTML;
-//   ++value;
-//   console.log(value);
-//   document.getElementById("incrimentText3").innerHTML = value;
-// };
+// COUNTER OF LIKES - IncrimentHEART PROBA3
+function incrimentButton3() {
+  let element = document.getElementById("incrimentText3");
+  let value = element.innerHTML;
+  ++value;
+  console.log(value);
+  document.getElementById("incrimentText3").innerHTML = value;
+};
