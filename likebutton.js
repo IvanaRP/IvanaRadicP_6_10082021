3a84d5f47fe2088138da49b093ec131e5094ab72
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-10-04 00:18:43 +0200

    like
    like button corection

--- likebutton.js
+++ likebutton.js
@@ -1,39 +1,29 @@
 
 
 // like data atributesHEART
-
 const likes = document.querySelectorAll("li");
-
 likes.forEach((like) => {
   like.addEventListener("click", addLike);
 });
-
 function addLike() {
   this.dataset.choisi = this.dataset.choisi == "true" ? "false" : "true";
   calculLike();
   calculTotalLike();
 }
-
 // let likeNumber = document.getElementById(".likeNumber");
  function calculLike(){
   const likeNumber = document.querySelectorAll('li[data-choisi="true"]').length;
   document.querySelector("#countheart").textContent = likeNumber;
 }
-
-
-
-
-
 function calculTotalLike() {
   const choseLike = Array.from(
     document.querySelectorAll('li[data-choisi="true"]')
   );
-
   const totalLikes = choseLike.reduce(
     (total, like) => total + Number(like.dataset.calories),
     0
   );
-
   document.querySelector("#totalheart").textContent = totalLikes;
 }
+
 
