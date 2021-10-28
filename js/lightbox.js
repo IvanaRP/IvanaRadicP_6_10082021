ef39bb6f376d493981721d80a491ec37140f7a8f
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-10-28 13:23:55 +0200

    lightbox
    lightbox video

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,226 +1,272 @@
-
-
 // LIGHTBOX
 // function lightbox() {
-  
-    import {enableBodyScroll, disableBodyScroll} from './body-scroll-lock.js' //for scroll bar
-    
-    
-    /**
-     *
-     * @property {HTMLElement} element
-     * @property {string[]} images links to the images of the lightbox
-     * @property {string} url images showed
-     */
-  
-    class Lightbox {
-      static init() {
-        const links = Array.from(
-          document.querySelectorAll('a[href$=".jpg"], a[href$=".mp4"]')
-        );
-       
-        // console.log(title);
-        const gallery = links.map((link) => ({
-          href : link.getAttribute("href"), title: link.getAttribute("alt")
-        }));
-      
-        links.forEach((link) =>
-          link.addEventListener("click", (e) => {
-            e.preventDefault();
-            new Lightbox(e.currentTarget.getAttribute("href"), gallery, e.currentTarget.getAttribute("alt"));
-          })
-        );
-      }
-  
-      /**
-       *
-       * @param {string} url  URL of the image
-       * @param {object[]} images  links to the images of the Lightbox
-       */
-  
-      constructor(url, images, title) {
-        this.element = this.buildDOM(url, title);
-        this.images = images;
-        this.loadImage({href:url, title:title});
-        this.onKeyUp = this.onKeyUp.bind(this);
-        document.body.appendChild(this.element);
-        disableBodyScroll(this.element);
-        document.addEventListener("keyup", this.onKeyUp);
-      }
-  
-      /**
-       *
-       * @param {string} url  URL of image
-       *
-       */
-  
-      // loadImage(imageObject) {
-      //   // console.log(imageObject);
-      //   this.url = null;
-      //   const image = new Image();
-      //   const container = this.element.querySelector(".lightbox__container");
-      //   const loader = document.createElement("div");
-      //   loader.classList.add("lightbox__loader");
-      //   container.innerHTML = "";
-      //   container.appendChild(loader);
-      //   image.onload = () => {
-      //     container.removeChild(loader);
-      //     container.appendChild(image);
-      //     this.url = imageObject.href;
-      //   };
-      //   image.src = imageObject.href;
-      //   this.element.querySelector(".lightbox__title").innerHTML = imageObject.title;
-      // }
 
-      loadImage(imageObject) {
-        // console.log(imageObject);
-        this.url = null;
-        const image = new Image();
-        const video = document.createElement("video");
-        const container = this.element.querySelector(".lightbox__container");
-        const loader = document.createElement("div");
-        loader.classList.add("lightbox__loader");
-        container.innerHTML = "";
-        container.appendChild(loader);
-        if(url.includes("jpg")){
-          image.onload = () => {
-            container.removeChild(loader);
-            container.appendChild(image);
-            this.url = imageObject.href;
-          };
-          image.src = imageObject.href;
-         
-        }
-        else if(url.includes("video")){
-          video.onload = () => {
-            container.removeChild(loader);
-            container.appendChild(video);
-            this.url = imageObject.href;
-          };
-          video.src = imageObject.href;
-        }
-      
-       
-       
-        this.element.querySelector(".lightbox__title").innerHTML = imageObject.title;
-      }
-      // loadImage(imageObject) {
-      //  // console.log(imageObject);
-      //   this.url = null;
-      //   const image = new Image();
-      //   const container = this.element.querySelector(".lightbox__container");
-      //   // const loader = document.createElement("div");
-      //   const video = document.createElement("video");
-      //   // loader.classList.add("lightbox__loader");
-      //   container.innerHTML = "";
-      //   // container.appendChild(loader);
-      //   image.onload = () => {
-      //     if (url.includes("jpg")) {
-      //           container.appendChild(image);
-      //           image.src = url;
-      //         } else if (url.includes('mp4')) {
-      //           container.appendChild(video);
-      //           video.src = url;
-      //         } 
-      //     this.url = imageObject.href;
-      //   };
-      //   image.src = imageObject.href;
-      //   video.src = imageObject.href;
-      //   this.element.querySelector(".lightbox__title").innerHTML = imageObject.title;
-      // }
-     
+import { enableBodyScroll, disableBodyScroll } from "./body-scroll-lock.js"; //for scroll bar
 
-      // loadImage(url) {
-      //   const image = new Image();
-      //   const video = document.createElement("video");
-      //   const container = this.element.querySelector(".lightbox__container");
-      //   container.innerHTML = "";
-      //   this.url = url;
-      //   if (url.includes("jpg")) {
-      //     container.appendChild(image);
-      //     image.src = url;
-      //   } else if (url.includes('mp4')) {
-      //     container.appendChild(video);
-      //     video.src = url;
-      //   }
-      // }
-  
-  
-      /**
-       *
-       * @param {KeyboardEvent} e
-       *
-       */
-  
-      onKeyUp(e) {
-        if (e.key === "Escape") {
-          this.close(e);
-        } else if (e.key === "ArrowLeft") {
-          this.prev(e);
-        } else if (e.key === "ArrowRight") {
-          this.next(e);
-        }
-      }
-  
-      /**
-       * Close lightbbox
-       * @param {MouseEvent/KeyboardEvent} e
-       *
-       */
-  
-      close(e) {
+/**
+ *
+ * @property {HTMLElement} element
+ * @property {string[]} images links to the images of the lightbox
+ * @property {string} url images showed
+ */
+
+class Lightbox {
+  static init() {
+    const links = Array.from(
+      document.querySelectorAll('a[href$=".jpg"], a[href$=".mp4"]')
+    );
+
+    // console.log(title);
+    const gallery = links.map((link) => ({
+      href: link.getAttribute("href"),
+      title: link.getAttribute("alt"),
+    }));
+
+    links.forEach((link) =>
+      link.addEventListener("click", (e) => {
         e.preventDefault();
-        this.element.classList.add("fadeOut");
-        enableBodyScroll(this.element);
-        window.setTimeout(() => {
-          this.element.parentElement.removeChild(this.element);
-        }, 500);
-        document.removeEventListener("keyup", this.onKeyUp);
-      }
+        new Lightbox(
+          e.currentTarget.getAttribute("href"),
+          gallery,
+          e.currentTarget.getAttribute("alt")
+        );
+      })
+    );
+  }
+
+  /**
+   *
+   * @param {string} url  URL of the image
+   * @param {object[]} images  links to the images of the Lightbox
+   */
+
+  constructor(url, images, title) {
+    this.element = this.buildDOM(url, title);
+    this.images = images;
+    this.loadImage({ href: url, title: title });
+    this.onKeyUp = this.onKeyUp.bind(this);
+    document.body.appendChild(this.element);
+    disableBodyScroll(this.element);
+    document.addEventListener("keyup", this.onKeyUp);
+  }
+
+  /**
+   *
+   * @param {string} url  URL of image
+   *
+   */
+
+  //  loadImage(imageObject) {
+  //   // console.log(imageObject);
+  //   this.url = null;
+  //   const image = new Image();
+  //    console.log(image);
+  //   const container = this.element.querySelector(".lightbox__container");
+  //   const loader = document.createElement("div");
+  //   loader.classList.add("lightbox__loader");
+  //   container.innerHTML = "";
+  //   container.appendChild(loader);
+  //   image.onload = () => {
+  //     container.removeChild(loader);
+  //     container.appendChild(image);
+  //     this.url = imageObject.href;
+  //   };
+  //   image.src = imageObject.href;
+  //   this.element.querySelector(".lightbox__title").innerHTML = imageObject.title;
+  // }
+
+
+  // loadImage(imageObject) {
+  //   // console.log(imageObject);
+  //   this.url = null;
+  //   // console.log(this.url);
+  //   const image = new Image();
+  //    console.log(image);
+  //   //  const video = new Video();
+  //   //  console.log(video);
+  //   const container = this.element.querySelector(".lightbox__container");
+  //   const loader = document.createElement("div");
+  //   loader.classList.add("lightbox__loader");
+  //   container.innerHTML = "";
+  //   container.appendChild(loader);
+  //   image.onload = () => {
+  //     container.removeChild(loader);
+  //     container.appendChild(image);
+  //     this.url = imageObject.href;
+  //   };
+  //   image.src = imageObject.href;
+   
+    
+  //   this.element.querySelector(".lightbox__title").innerHTML = imageObject.title;
+  // }
+
+  // loadImage(imageObject) {
+  //   // console.log(imageObject);
+    
+  //   this.url = null;
+  //   if(href.indexOf('mp4')) {
+  //       console.log('url contains "mp4"');
+  //   }
+  //   const image = new Image();
+  //   const container = this.element.querySelector(".lightbox__container");
+ 
+  //   const loader = document.createElement("div");
+  //   loader.classList.add("lightbox__loader");
+  //   container.innerHTML = "";
+  //   container.appendChild(loader);
+  //   image.onload = () => {
+  //         container.removeChild(loader);
+  //         container.appendChild(image);
+  //         this.url = imageObject.href;
+  //       };
+  //       image.src = imageObject.href;
+  //       this.element.querySelector(".lightbox__title").innerHTML = imageObject.title;
+  //     }
   
-      /**
-       * Close lightbbox
-       * @param {MouseEvent/KeyboardEvent} e
-       *
-       */
-  
-      next(e) {
-        e.preventDefault();
-        let i = this.images.findIndex((image) => image.href === this.url);
-        
-        if (i === this.images.length - 1) {
-          i = -1;
-        }
-        console.log(i);
-        console.log(this.images[i + 1]);
-        this.loadImage(this.images[i + 1]);
-      }
-  
-      /**
-       * Close lightbbox
-       * @param {MouseEvent/KeyboardEvent} e
-       *
-       */
-  
-      prev(e) {
-        e.preventDefault();
-        let i = this.images.findIndex((image) => image.href === this.url);
-        if (i === 0) {
-          i = this.images.length;
-        }
-        this.loadImage(this.images[i - 1]);
-      }
-  
-      /**
-       *
-       * @param {string} url  url of image
-       * @return {HTMLElement}
-       */
-  
-      buildDOM(url, title) {
-        const dom = document.createElement("div");
-        dom.classList.add("lightbox");
-        dom.innerHTML = `
+
+  loadImage(imageObject) {
+    // console.log(imageObject);
+    this.url = imageObject.href;
+    const image = new Image();
+    const video = document.createElement("video");
+    const container = this.element.querySelector(".lightbox__container");
+    const loader = document.createElement("div");
+    loader.classList.add("lightbox__loader");
+    container.innerHTML = "";
+    container.appendChild(loader);
+    if(this.url.includes("jpg")){
+      image.onload = () => {
+        container.removeChild(loader);
+        container.appendChild(image);
+        this.url = imageObject.href;
+      };
+      image.src = imageObject.href;
+    }
+    else if(this.url.includes("video")){
+      video.onload = () => {
+        container.removeChild(loader);
+        container.appendChild(video);
+        this.url = imageObject.href;
+      };
+      video.src = imageObject.href;
+    }
+    this.element.querySelector(".lightbox__title").innerHTML = imageObject.title;
+  }
+  // loadImage(imageObject) {
+  //  // console.log(imageObject);
+  //   this.url = null;
+  //   const image = new Image();
+  //   const container = this.element.querySelector(".lightbox__container");
+  //   // const loader = document.createElement("div");
+  //   const video = document.createElement("video");
+  //   // loader.classList.add("lightbox__loader");
+  //   container.innerHTML = "";
+  //   // container.appendChild(loader);
+  //   image.onload = () => {
+  //     if (url.includes("jpg")) {
+  //           container.appendChild(image);
+  //           image.src = url;
+  //         } else if (url.includes('mp4')) {
+  //           container.appendChild(video);
+  //           video.src = url;
+  //         }
+  //     this.url = imageObject.href;
+  //   };
+  //   image.src = imageObject.href;
+  //   video.src = imageObject.href;
+  //   this.element.querySelector(".lightbox__title").innerHTML = imageObject.title;
+  // }
+
+  // loadImage(url) {
+  //   const image = new Image();
+  //   const video = document.createElement("video");
+  //   const container = this.element.querySelector(".lightbox__container");
+  //   container.innerHTML = "";
+  //   this.url = url;
+  //   if (url.includes("jpg")) {
+  //     container.appendChild(image);
+  //     image.src = url;
+  //   } else if (url.includes('mp4')) {
+  //     container.appendChild(video);
+  //     video.src = url;
+  //   }
+  // }
+
+  /**
+   *
+   * @param {KeyboardEvent} e
+   *
+   */
+
+  onKeyUp(e) {
+    if (e.key === "Escape") {
+      this.close(e);
+    } else if (e.key === "ArrowLeft") {
+      this.prev(e);
+    } else if (e.key === "ArrowRight") {
+      this.next(e);
+    }
+  }
+
+  /**
+   * Close lightbbox
+   * @param {MouseEvent/KeyboardEvent} e
+   *
+   */
+
+  close(e) {
+    e.preventDefault();
+    this.element.classList.add("fadeOut");
+    enableBodyScroll(this.element);
+    window.setTimeout(() => {
+      this.element.parentElement.removeChild(this.element);
+    }, 500);
+    document.removeEventListener("keyup", this.onKeyUp);
+  }
+
+  /**
+   * Close lightbbox
+   * @param {MouseEvent/KeyboardEvent} e
+   *
+   */
+
+  next(e) {
+    e.preventDefault();
+    let i = this.images.findIndex((image) => image.href === this.url);
+
+    if (i === this.images.length - 1) {
+      i = -1;
+    }
+    console.log(i);
+    console.log(this.images[i + 1]);
+    this.loadImage(this.images[i + 1]);
+  }
+
+  /**
+   * Close lightbbox
+   * @param {MouseEvent/KeyboardEvent} e
+   *
+   */
+
+  prev(e) {
+    e.preventDefault();
+    let i = this.images.findIndex((image) => image.href === this.url);
+    if (i === 0) {
+      i = this.images.length;
+    }
+    this.loadImage(this.images[i - 1]);
+  }
+
+  /**
+   *
+   * @param {string} url  url of image
+   * @return {HTMLElement}
+   */
+
+  buildDOM(url, title) {
+    const dom = document.createElement("div");
+    dom.classList.add("lightbox");
+    dom.innerHTML = `
             <button class="lightbox__close">Close</button>
             <button class="lightbox__next">Next</button>
             <button class="lightbox__prev">Prev</button>
@@ -230,21 +276,18 @@
             <p class="lightbox__title">${title}</p>
             </div>
            `;
-        dom
-          .querySelector(".lightbox__close")
-          .addEventListener("click", this.close.bind(this));
-        dom
-          .querySelector(".lightbox__next")
-          .addEventListener("click", this.next.bind(this));
-        dom
-          .querySelector(".lightbox__prev")
-          .addEventListener("click", this.prev.bind(this));
-        return dom;
-      }
-    }
-  
-    Lightbox.init();
-//  
+    dom
+      .querySelector(".lightbox__close")
+      .addEventListener("click", this.close.bind(this));
+    dom
+      .querySelector(".lightbox__next")
+      .addEventListener("click", this.next.bind(this));
+    dom
+      .querySelector(".lightbox__prev")
+      .addEventListener("click", this.prev.bind(this));
+    return dom;
+  }
+}
 
-
-
+Lightbox.init();
+//
